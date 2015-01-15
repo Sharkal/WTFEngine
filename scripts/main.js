87e4dc818626d3229e59f2424921f3ba57feb2c4
Sharkal <maxi.bezold@gmail.com>
2015-01-15 15:46:15 +0100

    Oraa style

--- scripts/main.js
+++ scripts/main.js
@@ -38,4 +38,91 @@ $(function() {
     // @see https://support.google.com/drive/answer/37579?hl=en
     WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
 
+	// Populate using an object literal
+
+     WTF.init({
+
+         heading: [
+             "Yeah! Ein verdammter",
+             "Kuckst du! Das ist ein"
+         ],
+         response: [
+             "Kennt man doch schon...",
+             "Ach, leck mich doch!",
+             "Geh sterben!"
+         ],
+         template: [
+             "@adjective @race @profession",
+             "aus @location, der",
+			 "@backstory"
+         ],
+         adjective: [
+             "fetter",
+             "kleiner",
+             "naiver",
+			 "spindeldürrer",
+			 "wahnsinniger",
+			 "ängstlicher",
+			 "rassistischer",
+			 "aufbrausender",
+			 "vornehmer",
+			 "rechtschaffener",
+         ],
+         race: [
+             "Varaz",
+             "Oranir",
+             "Ukgnat"
+         ],
+		 profession: [
+             "Soldat",
+			 "Söldner",
+			 "Barbar",
+			 "Stammeskrieger",
+             "Myramit",
+			 "Meuchelklinge",
+			 "Giftmischer",
+			 "Schatten",
+             "Alchemist",
+			 "Druide",
+			 "Schamane",
+			 "Priester",
+			 "Jäger",
+			 "Schmied",
+			 "Barde",
+			 "Tierfänger",
+			 "Erfinder",
+			 "Schatzjäger",
+			 "Pirat",
+			 "Dieb",
+			 "Myotänzer (Feuer)",
+			 "Myotänzer (Wasser)",
+			 "Myotänzer (Erde)",
+			 "Myotänzer (Luft)",
+			 "Myotänzer (Botanik)",
+			 "Myotänzer (Beherrschung)",
+			 "Myotänzer (Illusion)",
+			 "Myotänzer (Existenz)",
+			 "Myotänzer (Beschwörung)",
+			 "Myotänzer (Anatomik)",
+			 "Myotänzer (Dimentorik)",
+			 "Myotänzer (Runetik)",
+			 "Myotänzer (Stimulatorik)",
+			 "Myotänzer (Sensorik)",
+			 "Myotänzer (Konservik)"
+         ]
+		 location: [
+             "der Hauptstadt Oitaria",
+             "Reiffels",
+             "der Mangrovenstadt Mangora",
+			 "einem kleinen Jägerdorf",
+			 "den Sümpfen von Bayriill",
+			 "dem gesetzlosen Ulmaret",
+			 "den Nomadenstämmen von Kaheli",
+         ],
+		 backstory: [
+             "Varaz",
+             "Oranir",
+             "Ukgnat"
+         ]
+     });
 });
